857db467f664f02a830ae44e7090c4cbed6845c5
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-02 11:26:18 +0700

    menambahkan output

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -16,6 +16,7 @@ void addNode() {
     Node* newNode = new Node(); // step 1: create a new node
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs;      // Assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
 
 }
 
