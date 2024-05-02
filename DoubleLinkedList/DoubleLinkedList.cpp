360d3496905a10756c377e1d4b837d1bccff06dc
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-02 11:27:38 +0700

    menambahkan statement if

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -18,6 +18,10 @@ void addNode() {
     cin >> newNode->noMhs;      // Assign value to the data field of the new node
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;       // Assign value to the data field of the new node
+    //insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs) {
+
+    }
 }
 
 int main()
